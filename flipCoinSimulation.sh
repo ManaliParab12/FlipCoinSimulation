7e8df82e74a78a4f0a9794da4fd10b066bb54f2b
ManaliParab12 <manaliparab10@gmail.com>
2020-10-14 15:39:22 +0530

    Used loop for multiple flipping to show number of times head and tail won

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,13 +1,21 @@
 #!/bin/bash -x
 
 echo "Welcome to Flip Coin Simulation"
+heads=0
+tails=0
 
-coin=$((RANDOM%2))
+for (( i=1; i<=20; i++ ))
+do
 
-if [ $coin -eq 1 ]
-then
-    echo "You got Heads"
-else
-    echo "You got Tails"
-fi
+    coin=$((RANDOM%2))
 
+    if [ $coin -eq 1 ]
+    then
+        heads=$(($heads+1))
+    else
+        tails=$(($tails+1))
+    fi
+done
+
+echo "Number of times heads won " $heads
+echo "Number of times Tails won " $tails
